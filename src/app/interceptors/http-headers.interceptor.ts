ee2945a389738a8a1268a073ef04193cb69e72e1
OrthofeetDerek <dereknashdev@gmail.com>
2022-11-02 13:01:07 -0400

    initial commit

---
+++ src/app/interceptors/http-headers.interceptor.ts
@@ -0,0 +1,24 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+
+@Injectable()
+export class HttpHeadersInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    req = req.clone({
+      setHeaders: {
+        'x-rapidapi-key': 'esGbwrm390mshS2BCl0RALxQRtZTp1W7sFMjsnyJlJzDXVkW0H',
+        'x-rapidapi-host': 'rawg-video-games-database.p.rapidapi.com'
+      },
+      setParams: {
+        key: 'f75072b8140c4b4c9725af1a59d83a78'
+      }
+    });
+    return next.handle(req);
+  }
+}
